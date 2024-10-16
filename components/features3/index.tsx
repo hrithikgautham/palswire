10896729854f160aec66af1ceaa2612d4c6452f1
Hrithik <hrithikgauthamtg@gmail.com>
2024-10-17 03:37:58 +0530

    initial commit

---
+++ components/features3/index.tsx
@@ -0,0 +1,110 @@
+import {Card, Divider, Text} from '@nextui-org/react';
+import React from 'react';
+import {BoxIcon} from '../icons/BoxIcon';
+import {Box} from '../styles/box';
+import {Flex} from '../styles/flex';
+
+export const Features3 = () => {
+   return (
+      <>
+         <Box
+            css={{
+               px: '$6',
+               pb: '$14',
+            }}
+         >
+            <Flex
+               direction={'column'}
+               justify={'center'}
+               align={'center'}
+               css={{
+                  pt: '$20',
+               }}
+            >
+               <Text span css={{color: '$blue600'}}>
+                  Features
+               </Text>
+               <Text h3>Palswire Helps you connect with the right people</Text>
+            </Flex>
+            <Flex
+               align={'center'}
+               justify={'center'}
+               wrap={'wrap'}
+               css={{
+                  gap: '1rem',
+                  pt: '$14',
+               }}
+            >
+               <Card css={{mw: '500px'}}>
+                  <Card.Body>
+                     <Flex css={{gap: '0.5rem'}}>
+                        <BoxIcon />
+                        <Flex direction={'column'}>
+                           <Text h5>Connect Closeby</Text>
+                           <Text span>
+                              Connect with people in your locality
+                           </Text>
+                        </Flex>
+                     </Flex>
+                  </Card.Body>
+               </Card>
+               <Card css={{mw: '500px'}}>
+                  <Card.Body>
+                     <Flex css={{gap: '0.5rem'}}>
+                        <BoxIcon />
+                        <Flex direction={'column'}>
+                           <Text h5>Your'e not alone</Text>
+                           <Text span>
+                              You always have someone to accompany you
+                           </Text>
+                        </Flex>
+                     </Flex>
+                  </Card.Body>
+               </Card>
+            </Flex>
+            <Flex
+               align={'center'}
+               wrap={'wrap'}
+               justify={'center'}
+               css={{
+                  gap: '1rem',
+                  pt: '$8',
+               }}
+            >
+               <Card css={{mw: '500px'}}>
+                  <Card.Body>
+                     <Flex css={{gap: '0.5rem'}}>
+                        <BoxIcon />
+                        <Flex direction={'column'}>
+                           <Text h5>Your Title</Text>
+                           <Text span>
+                              fsdfs
+                           </Text>
+                        </Flex>
+                     </Flex>
+                  </Card.Body>
+               </Card>
+               <Card css={{mw: '500px'}}>
+                  <Card.Body>
+                     <Flex css={{gap: '0.5rem'}}>
+                        <BoxIcon />
+                        <Flex direction={'column'}>
+                           <Text h5>Your Title</Text>
+                           <Text span>
+                              Lorem ipsum dolor sit amet, consectetur adipiscing
+                              elit. Sed condimentum, nisl ut aliquam lacinia,
+                              nisl nisl aliquet nisl, nec
+                           </Text>
+                        </Flex>
+                     </Flex>
+                  </Card.Body>
+               </Card>
+            </Flex>
+         </Box>
+
+         <Divider
+            css={{position: 'absolute', inset: '0p', left: '0', mt: '$5'}}
+         />
+      </>
+   );
+};
